bee7a0eb90ffe77994ac2d9729d322eba8dde2e7
friendlyanon <1736896+friendlyanon@users.noreply.github.com>
2020-08-20 22:34:37 +0200

    Fix pagination issue and add separator between posts

--- karriereat-crawler.user.js
+++ karriereat-crawler.user.js
@@ -2,7 +2,7 @@
 // @name        karriere.at crawler
 // @namespace   friendlyanon
 // @match       https://www.karriere.at/*
-// @version     2
+// @version     3
 // @grant       GM_addStyle
 // @grant       GM_setValue
 // @grant       GM_getValue
@@ -16,6 +16,7 @@
 GM_addStyle(`
 .m-jobsListItem__titleLink { background-color: rgba(255, 255, 255, 0.5); }
 .m-jobsListItem__titleLink:visited { background-color: rgba(255, 0, 0, 0.5); }
+.day-separator { border-top: 30px red solid; }
 `);
 
 class MenuItem {
@@ -89,18 +90,30 @@ class Search {
         }
 
         const img = card.querySelector(".m-jobsListItem__logo");
-        img.src = img.dataset.latesrc;
+        if (img != null) {
+          img.src = img.dataset.latesrc;
+        }
+
         const [day, month, year] = card
           .querySelector(".m-jobsListItem__date")
           .textContent.slice(3).split(".");
+        const date = `${year}-${month}-${day}`;
 
-        yield [new Date(`${year}-${month}-${day}`).getTime(), card];
+        yield [new Date(date).getTime(), [date, card]];
       }
     }
   }
 
   static sortByDate([a], [b]) {
     return b - a;
+  }
+
+  static *filterPage(page) {
+    for (const card of page) {
+      if (card.classList.length === 1) {
+        yield card;
+      }
+    }
   }
 
   static async *crawl(region, job) {
@@ -126,7 +139,7 @@ class Search {
       },
     };
 
-    for (let i = 1; ; ++i) {
+    for (let i = 1, limit = 1; ; ++i) {
       if (i > 1) {
         options.url = `${base}?page=${i}`;
       }
@@ -135,10 +148,15 @@ class Search {
       GM_xmlhttpRequest(options);
       console.log("Fetching %s, page %d", job, i);
       const doc = await promise;
+      if (i === 1) {
+        const [meta] = doc.getElementsByClassName("m-pagination__meta");
+        const { 2: max } = meta.textContent.trim().split(" ");
+        limit = parseInt(max, 10);
+      }
 
-      yield [...doc.querySelectorAll("li[class='m-jobsList__item']")];
+      yield [...Search.filterPage(doc.getElementsByClassName("m-jobsList__item"))];
 
-      if (doc.querySelector(".m-pagination__button--next.m-pagination__button--disabled")) {
+      if (!(i < limit)) {
         break;
       }
     }
@@ -161,8 +179,17 @@ class Search {
 
     const relevantCards = [...Search.process(results)];
     const target = document.querySelector("ol.m-jobsList");
-    for (const [, card] of relevantCards.sort(Search.sortByDate)) {
+    let previousDate = null;
+    for (const [, [date, card]] of relevantCards.sort(Search.sortByDate)) {
       target.appendChild(card);
+
+      if (previousDate !== date) {
+        if (previousDate != null) {
+          card.classList.add("day-separator");
+        }
+
+        previousDate = date;
+      }
     }
 
     this.working = false;
